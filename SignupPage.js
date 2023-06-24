424c14c42d7daec8e3defe302103a0dd480b7c64
raagzi <114258169+raagzi@users.noreply.github.com>
2023-06-24 22:07:48 +0530

    Create SignupPage.js

---
+++ SignupPage.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send signup request to server or handle user registration
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
